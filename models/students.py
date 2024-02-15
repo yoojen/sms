b5ed66413c2a28fa1042bb47e45984dae349361e
Eugene <eugenemutuyimana15@gmail.com>
2024-02-15 14:44:51 +0200

    fixed import issue

--- models/students.py
+++ models/students.py
@@ -1,7 +1,6 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
-from base_model import Base, BaseModel
-from datetime import datetime, date
+from models.base_model import Base, BaseModel
 
 
 class Student(Base, BaseModel):
@@ -12,12 +11,12 @@ class Student(Base, BaseModel):
     last_name = Column(String(50))
     email = Column(String(50), nullable=False)
     password = Column(String(18), nullable=False)
-    dob = Column(date, nullable=False)
+    dob = Column(DateTime, nullable=False)
     dept_id = Column(String(50), ForeignKey("departments.id"), nullable=False)
     year_of_study = Column(Integer, nullable=False)
     sponsorship = Column(String(50))
     citizenship = Column(String(50), nullable=False)
-    last_login = Column(datetime)
+    last_login = Column(DateTime)
 
     scores = relationship("Score", back_populates="student")
     submissions = relationship("Submission", back_populates="student")
