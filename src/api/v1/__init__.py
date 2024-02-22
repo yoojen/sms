0e78ad99da884ffcdf8a6f463af37edd0c67ee86
Eugene <eugenemutuyimana15@gmail.com>
2024-02-23 00:06:39 +0200

    imported roles blueprint

--- src/api/v1/__init__.py
+++ src/api/v1/__init__.py
@@ -3,3 +3,4 @@ from api.v1.views.departments import *
 from api.v1.views.assingments import *
 from api.v1.views.communications import *
 from api.v1.views.materials import *
+from api.v1.views.roles_n_admins import *
