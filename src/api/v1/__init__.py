a3d43ac1e33eaab4e841bffe83ac7c1e930db053
Eugene <eugenemutuyimana15@gmail.com>
2024-02-24 15:54:26 +0200

    importing tchr_dept endpoints

--- src/api/v1/__init__.py
+++ src/api/v1/__init__.py
@@ -9,3 +9,4 @@ from api.v1.views.students import *
 from api.v1.views.submissions import *
 from api.v1.views.teachers import *
 from api.v1.views.degrees import *
+from api.v1.views.teacher_dept import *
